6a4d9dfe2e9941849c69581ef0959f9ac4d9b268
Victor Bombi <sonoroster@gmail.com>
2020-10-02 11:08:48 +0200

    initial commit

---
+++ generator/cimnodes_template.cpp
@@ -0,0 +1,10 @@
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "./imnodes/imnodes.h"
+#include "cimnodes.h"
+
+
+
+#include "auto_funcs.cpp"
+
+
---
+++ generator/output/typedefs_dict.lua
@@ -0,0 +1,8 @@
+local defs = {}
+defs["EmulateThreeButtonMouse"] = "struct EmulateThreeButtonMouse"
+defs["IO"] = "struct IO"
+defs["ImVec2"] = "struct ImVec2"
+defs["LinkDetachWithModifierClick"] = "struct LinkDetachWithModifierClick"
+defs["Style"] = "struct Style"
+
+return defs
