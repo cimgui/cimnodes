3ff05a1ec8396fc128f19577192ebf5efe65c266
sonoro1234 <sonoroster@gmail.com>
2025-01-08 10:54:41 +0100

    new serializer

--- generator/output/typedefs_dict.lua
+++ generator/output/typedefs_dict.lua
@@ -1,20 +1,19 @@
-local defs = {}
-defs["EmulateThreeButtonMouse"] = "struct EmulateThreeButtonMouse"
-defs["ImGuiContext"] = "struct ImGuiContext"
-defs["ImNodesAttributeFlags"] = "int"
-defs["ImNodesCol"] = "int"
-defs["ImNodesContext"] = "struct ImNodesContext"
-defs["ImNodesEditorContext"] = "struct ImNodesEditorContext"
-defs["ImNodesIO"] = "struct ImNodesIO"
-defs["ImNodesMiniMapLocation"] = "int"
-defs["ImNodesMiniMapNodeHoveringCallback"] = "void (*)(int, void*);"
-defs["ImNodesMiniMapNodeHoveringCallbackUserData"] = "void*"
-defs["ImNodesPinShape"] = "int"
-defs["ImNodesStyle"] = "struct ImNodesStyle"
-defs["ImNodesStyleFlags"] = "int"
-defs["ImNodesStyleVar"] = "int"
-defs["ImVec2"] = "struct ImVec2"
-defs["LinkDetachWithModifierClick"] = "struct LinkDetachWithModifierClick"
-defs["MultipleSelectModifier"] = "struct MultipleSelectModifier"
-
-return defs
+local t={
+  EmulateThreeButtonMouse="struct EmulateThreeButtonMouse",
+  ImGuiContext="struct ImGuiContext",
+  ImNodesAttributeFlags="int",
+  ImNodesCol="int",
+  ImNodesContext="struct ImNodesContext",
+  ImNodesEditorContext="struct ImNodesEditorContext",
+  ImNodesIO="struct ImNodesIO",
+  ImNodesMiniMapLocation="int",
+  ImNodesMiniMapNodeHoveringCallback="void (*)(int, void*);",
+  ImNodesMiniMapNodeHoveringCallbackUserData="void*",
+  ImNodesPinShape="int",
+  ImNodesStyle="struct ImNodesStyle",
+  ImNodesStyleFlags="int",
+  ImNodesStyleVar="int",
+  ImVec2="struct ImVec2",
+  LinkDetachWithModifierClick="struct LinkDetachWithModifierClick",
+  MultipleSelectModifier="struct MultipleSelectModifier"}
+ return t
